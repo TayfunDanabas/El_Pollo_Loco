ee91a78133854dc58f9e69924b1a1010683066fb
TayfunDanabas <danabas285@gmail.com>
2026-07-16 23:50:41 +0200

    first commit

---
+++ models/chicken.class.js
@@ -0,0 +1,33 @@
+class Chicken extends MovableObject {
+
+    y = 350;
+    height = 60;
+    width = 100;
+    IMAGES_WALKING =[
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+        
+    ];
+
+    constructor(){
+        super().loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.x = 200 + Math.random() * 500;
+        this.speed = 0.15 + Math.random() + 0.5;
+        this.animate();
+    }
+
+
+
+
+    animate() {
+        this.moveLeft();
+
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 200);
+    }
+
+}
